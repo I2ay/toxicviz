f0917b3882d44a1dcc6dce4dfc3f99f31063cb50
I2ay <ray.cd.tang8@gmail.com>
2020-09-25 21:23:43 +1000

    Init commit

---
+++ API_Flask/api.py
@@ -0,0 +1,48 @@
+#!flask/bin/python
+from flask import Flask
+# required imports
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.text import tokenizer_from_json 
+from tensorflow.keras.models import model_from_json
+import json
+
+app = Flask(__name__)
+
+# load model architecture
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+model = model_from_json(loaded_model_json)
+
+# load model weights
+model.load_weights("model_weights.h5")
+
+# load tokenizer
+json_file = open('tokenizer.json', 'r')
+loaded_tokenizer_json = json_file.read()
+json_file.close()
+loaded_tokenizer = tokenizer_from_json(loaded_tokenizer_json)
+max_length=1342
+
+def predict_toxicity(comment):
+    # process
+    Xnew_processed=pad_sequences(loaded_tokenizer.texts_to_sequences([comment]),maxlen=max_length, padding='post')
+    # predict
+    return [model.predict(Xnew_processed)[i][0][0] for i in range(6)]
+
+def prediction_to_json(pred):
+    return {
+     'toxic': str(pred[0]),
+     'severe_toxic': str(pred[1]),
+     'obscene': str(pred[2]),
+     'threat': str(pred[3]),
+     'insult': str(pred[4]),
+     'identity_hate': str(pred[5])
+    }
+
+@app.route('/<comment>')
+def index(comment):
+    toxic=predict_toxicity(comment)
+    return json.dumps(prediction_to_json(toxic))
+
+if __name__ == '__main__':
+    app.run(debug=True, threaded=True)
---
+++ FrontEnd_Electron/renderer.js
@@ -0,0 +1,227 @@
+let Chart = require('chart.js');
+let labels = ['Toxic', 'Severely Toxic', 'Obscene', 'Threat', 'Insult', 'Identity Hate'];
+
+function get(comment) {
+    return new Promise ((resolve, reject) => {
+        setTimeout(() => {
+            fetch('http://127.0.0.1:5000/'+comment)
+            .then(res => {
+                resolve(res);
+            })
+            .catch(err =>{
+                reject(err);
+            });
+        })
+    })     
+}
+
+
+function toPercent(str, places=2) {
+    let num = parseFloat(str);
+    str = (num * 100).toFixed(places);
+    return str+'%';
+}
+
+let ctx = document.getElementById('barChart').getContext('2d');
+let barChart = new Chart(ctx, {
+    type: 'horizontalBar',
+    data: {
+        labels: labels,
+        datasets: [{
+            data: [],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        legend: {display: false},
+        scales: {
+            xAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                    max: 1,
+                    // Include a % sign in the ticks
+                    callback: function(value, index, values) {
+                        return toPercent(value,0);
+                    }
+                }
+            }]
+        },
+        tooltips: {
+            callbacks: {
+                // Change to %
+                label: function(context) {
+                    let value = context.value || '';
+                    
+                    return toPercent(value);
+                }
+            }
+        }
+    }
+});
+
+
+let ctxRdr = document.getElementById('radarChart').getContext('2d');
+let radarChart = new Chart(ctxRdr, {
+    type: 'radar',
+    data: {
+        labels: labels,
+        datasets: []
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        // legend: {display: false},
+        scale: {
+            ticks: {
+                startAtZero: true,
+                max: 1,
+                // Include a % sign in the ticks
+                callback: function(value, index, values) {
+                    return toPercent(value,0);
+                }
+            }
+        },
+        tooltips: {
+            callbacks: {
+                // Change to %
+                label: function(context) {
+                    let value = context.value || '';                    
+                    return labels[context.index]+": "+toPercent(value);
+                },
+                title: function(context, data) {
+                    return data.datasets[context[0].datasetIndex].label;
+                }
+            }
+        }
+    }
+});
+
+let pointBackgroundColors=[];
+let ctxLn = document.getElementById('lineChart').getContext('2d');
+let lineChart = new Chart(ctxLn, {
+    type: 'line',
+    data: {
+        datasets: [{
+            data:[],
+            backgroundColor: "rgba(200,0,0,0.4)",            
+            pointBackgroundColor: pointBackgroundColors,
+            fill: false
+        }]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        legend: {display: false},
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                    max: 1,
+                    // Include a % sign in the ticks
+                    callback: function(value, index, values) {
+                        return toPercent(value,0);
+                    }
+                }
+            }]
+        },
+        tooltips: {
+            callbacks: {
+                // Change to %
+                label: function(context) {
+                    let value = context.value || '';
+                    
+                    return toPercent(value);
+                }
+            }
+        }
+    }
+});
+
+function addEntry(chart, data, comment) {
+    chart.data.labels.push(comment);
+    chart.data.datasets[0].data.push(data);
+    let r = data*255;
+    pointBackgroundColors.push(`rgba(${r},${255-r},0,1)`)
+    chart.update();
+}
+
+
+function addData(chart, data, comment) {
+    let r = getAvg(data)*255;
+    let bgClr =`rgba(${r},${255-r},0,0.2)`;
+    let brdClr =`rgba(${r},${255-r},0,1)`;
+    let dataset = {
+        label: comment,        
+        backgroundColor: bgClr,
+        borderColor: brdClr,
+        data: data
+    }
+    chart.data.datasets.push(dataset);
+    chart.update();
+}
+
+function removeData(chart) {
+    // chart.data.labels.pop();
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.pop();
+    });
+    chart.update();
+}
+function editData(chart, data) {
+    chart.data.datasets[0].data = data;
+    chart.update();
+}
+
+function getAvg(data) {    
+    let total = 0;
+    for (let i =0; i< data.length; i++) {            
+        let num = parseFloat(data[i]);
+        total+=num;
+    }
+    return total/6;
+}
+
+const btnGet = document.getElementById('get');
+btnGet.addEventListener('click', function () {
+    let comment = document.getElementById("comment").innerHTML
+    get(encodeURIComponent(comment))
+    .then(res => res.json())
+    .then(res=>{
+        let data = [res.toxic, res.severe_toxic, res.obscene, res.threat, res.insult, res.identity_hate];
+        editData(barChart, data);//bar
+        total=getAvg(data);
+        addData(radarChart, data, comment);//radar
+        addEntry(lineChart, total, comment)//line
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
+const btnClear = document.getElementById('clear');
+btnClear.addEventListener('click', function () {    
+    radarChart.data.datasets = [];
+    radarChart.update();
+    barChart.data.datasets[0].data = [];
+    barChart.update();
+    lineChart.data.datasets[0].data = [];
+    lineChart.data.datasets[0].labels = [];
+    lineChart.update();
+})
