0dcf25791738d4361683a6f5c29e571f40f37212
Victoria <58699190+procrastinatress@users.noreply.github.com>
2020-09-28 21:51:52 +1000

    Started sentiment colouring of text

---
+++ FrontEnd_Electron/handle-text.js
@@ -0,0 +1,44 @@
+async function stringToArray() {
+
+    inputText = getInput();
+  
+    // split words into array
+    var inputArray = inputText.split(' '); 
+    var toxicness;
+
+    // Print out array of words
+    for (var i in inputArray){
+        toxicness = await computeToxicity(inputArray[i]);
+        if (toxicness>0.75){
+        document.getElementById("firstLine").innerHTML += "<span style='color: #FF0000;'>" + inputArray[i] + " </span> ";
+        }
+        else{
+        document.getElementById("firstLine").innerHTML += inputArray[i] + " ";
+        }
+    }
+}
+
+function computeToxicity(comment){
+    return new Promise(function(resolve){
+    
+        var toxicity;
+        get(encodeURIComponent(comment))
+        .then(res => res.json())
+        .then(res=>{
+            let data = [res.toxic, res.severe_toxic, res.obscene, res.threat, res.insult, res.identity_hate];
+            toxicity = data[0];
+            resolve(toxicity);
+        })
+        .catch(err=>{
+            console.log(err)
+        }) 
+        
+    })
+
+}
+// Returns text from textbox
+function getInput() {
+  return document.getElementById("comment").innerHTML;
+}
+
+
